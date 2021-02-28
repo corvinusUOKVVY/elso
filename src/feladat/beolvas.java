b8c5ee672e494a7b9aea7cbce2dc16d7845e40db
Dominik Szeles <dominik.szeles@urss.hu>
2021-02-28 22:24:07 +0100

    kesz feladat

--- src/feladat/beolvas.java
+++ src/feladat/beolvas.java
@@ -1,7 +1,23 @@
 package feladat;
 
+import java.io.*;
+
 public class beolvas {
-    public static void main(String[] args) {
-        System.out.println("hi");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("a.txt"));
+        String szoveg;
+        while ((szoveg = bufferedReader.readLine()) != null) {
+            System.out.println("Beolvasott szöveg: " + szoveg);
+            char[] chars = szoveg.toCharArray();
+            int valtozo;
+            String ujSzoveg = "";
+            for (int i = 0; i < chars.length; i++) {
+                valtozo = chars[i] + 1;
+                char ch = (char) valtozo;
+                ujSzoveg = ujSzoveg + ch;
+            }
+            System.out.println("Titkosított szöveg: " + ujSzoveg);
+        }
     }
 }
+
